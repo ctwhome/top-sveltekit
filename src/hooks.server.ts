fb499a950f3cd52cb901b435c1b96008727bd150
Jesús García <ctw@ctwhome.com>
2024-10-14 22:45:54 +0200

    disable auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,21 +1,21 @@
-// https://kit.svelte.dev/docs/hooks
-import { env } from '$env/dynamic/private';
-import { sequence } from '@sveltejs/kit/hooks';
-import { handleAuth } from "./auth";
-import { pool } from "$lib/db/db";
+// // https://kit.svelte.dev/docs/hooks
+// import { env } from '$env/dynamic/private';
+// import { sequence } from '@sveltejs/kit/hooks';
+// import { handleAuth } from "./auth";
+// import { pool } from "$lib/db/db";
 
-// Initialize the database connection
-// console.log('🎹 DATABASE_URL', env.DATABASE_URL, env.DB_USER);
+// // Initialize the database connection
+// // console.log('🎹 DATABASE_URL', env.DATABASE_URL, env.DB_USER);
 
-// Set the app.user_id in the database to enable row level security
-async function setAppUser({ event, resolve }) {
-  const session = await event.locals.getSession();
-  if (session?.user?.id) {
-    await pool.query('SELECT set_app_user($1)', [session.user.id]);
-  } else {
-    await pool.query('SELECT set_app_user(NULL)');
-  }
-  return resolve(event);
-}
+// // Set the app.user_id in the database to enable row level security
+// async function setAppUser({ event, resolve }) {
+//   const session = await event.locals.getSession();
+//   if (session?.user?.id) {
+//     await pool.query('SELECT set_app_user($1)', [session.user.id]);
+//   } else {
+//     await pool.query('SELECT set_app_user(NULL)');
+//   }
+//   return resolve(event);
+// }
 
-export const handle = sequence(handleAuth, setAppUser);
+// export const handle = sequence(handleAuth, setAppUser);
