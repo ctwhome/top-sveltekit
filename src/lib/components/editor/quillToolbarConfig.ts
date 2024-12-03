5e98210b902628e5aad6831b855c47ff950a2a99
Jesús García <ctw@ctwhome.com>
2024-12-03 12:10:58 +0100

    remove quill editor and related configurations; update dependencies

--- src/lib/components/editor/quillToolbarConfig.ts
+++
@@ -1,9 +0,0 @@
-export const toolbarOptions = [
-  [{ header: [1, 2, 3, false] }],
-  ['bold', 'italic', 'underline', 'strike'],
-  ['blockquote', 'code-block'],
-  [{ list: 'ordered' }, { list: 'bullet' }],
-  [{ color: [] }, { background: [] }],
-  ['link', 'image'],
-  ['clean']
-];
